9132b5662342e1664645a8936e396aa386e480ec
Chad Davis <dynamiccookies@users.noreply.github.com>
2022-09-01 10:30:33 -0500

    Adjusted line order
    Moved script line up next to other import

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
 		<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
 		<link rel='stylesheet' href='style.css'>
 		<script src='http://code.jquery.com/jquery-latest.min.js'></script>
+		<script src='script.js'></script>
 		<script>
 			var radarrApiUrl = '<?=base64_encode($radarr_api_url)?>';
 			var debug        = '<?=$debug?>';
@@ -33,7 +34,6 @@
 			var searchTerm   = '<?=$search_term?>';
 			var tmdbId       = '<?=$tmdb_id?>';
 		</script>
-		<script src='script.js'></script>
 		<title>Movie Search</title>
 	</head>
 	<body>
